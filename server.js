94b606b9253fd5a831ad63c6df6b21e06f43828e
spen <rtaylor4581@gmail.com>
2025-11-07 19:09:17 -0600

    init commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
+const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const session = require("express-session");
